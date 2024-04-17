3cc3969a076feca50dd127f28761bc19adf62baa
peterkim2014 <peterkim2014@gmail.com>
2024-04-17 17:20:36 -0400

    updtaed

--- flask_app/controllers/homes.py
+++ flask_app/controllers/homes.py
@@ -1,7 +1,31 @@
 from flask import render_template, redirect, request, flash, session
 from flask_app import app
+import re
+
+
+def is_mobile(user_agent):
+    # Regular expressions for common mobile device strings
+    mobile_patterns = [
+        "iphone", "ipod", "ipad", "android", "blackberry",
+        "windows phone", "nokia", "samsung", "mobile"
+    ]
+    for pattern in mobile_patterns:
+        if re.search(pattern, user_agent):
+            print(f"Detected mobile device: {user_agent}")
+            return True
+    print(f"Not a mobile device: {user_agent}")
+    return False
+
+
 
 
 @app.route('/')
 def landing_page():
-    return render_template("homepage.html")
+    user_agent = request.headers.get('User-Agent')
+    user_agent = user_agent.lower()
+
+    if is_mobile(user_agent):
+        return render_template('homepageMobile.html')
+    else:
+        return render_template("homepage.html")
+    
