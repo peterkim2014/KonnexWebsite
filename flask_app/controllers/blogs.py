90b6312335b70da7dd440f981aac293e34581c46
peterkim2014 <peterkim2014@gmail.com>
2024-09-14 11:58:12 -0400

    updated

--- flask_app/controllers/blogs.py
+++ flask_app/controllers/blogs.py
@@ -21,16 +21,31 @@ def blogs_home():
     user_agent = request.headers.get('User-Agent')
     user_agent = user_agent.lower()
     device_type = detect_device(user_agent)
-
     error_message = session.pop('error_message', None)
 
-    if device_type == True:
+    # Detect if a blog is selected from the query parameter
+    selected_blog_id = request.args.get('blog_id')
+
+    # Sample blog data for demonstration purposes
+    blogs = [
+        {'id': 1, 'title': 'Introduction to Konnex', 'content': 'Welcome to the Konnex Blog!'},
+        {'id': 2, 'title': 'The Future of Cryptocurrency', 'content': 'What does the future hold for crypto?'},
+        {'id': 3, 'title': 'Blockchain Basics', 'content': 'Understanding the basics of blockchain.'}
+    ]
+
+    # If a blog_id is provided, select the appropriate blog
+    selected_blog = None
+    if selected_blog_id:
+        selected_blog = next((blog for blog in blogs if blog['id'] == int(selected_blog_id)), None)
+
+    # Return the appropriate template based on device type
+    if device_type:
         if "ipad" in user_agent:
-            print("Ipad")
-            return render_template("blogTablet.html", error_message=error_message)
+            print("iPad detected")
+            return render_template("blogTablet.html", selected_blog=selected_blog, blogs=blogs, error_message=error_message)
         else:
-            print("Iphone")
-            return render_template("blogMobile.html", error_message=error_message)
+            print("Mobile device detected")
+            return render_template("blogMobile.html", selected_blog=selected_blog, blogs=blogs, error_message=error_message)
     else:
-        print("Desktop")
-        return render_template("blog.html", error_message=error_message)
+        print("Desktop detected")
+        return render_template("blog.html", selected_blog=selected_blog, blogs=blogs, error_message=error_message)
