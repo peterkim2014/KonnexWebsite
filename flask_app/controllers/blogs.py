1fedc5cf76406920661c896a064cea8421c2c58b
peterkim2014 <peterkim2014@gmail.com>
2024-09-14 21:41:52 -0400

    updated

--- flask_app/controllers/blogs.py
+++ flask_app/controllers/blogs.py
@@ -24,19 +24,63 @@ def blogs_home():
     device_type = detect_device(user_agent)
     error_message = session.pop('error_message', None)
 
+
+    blogs = [
+        {
+            'id': 1,
+            'title': 'Introduction to Konnex',
+            'header': 'Getting Started with Konnex',
+            'body': 'Konnex is a revolutionary cryptocurrency wallet designed for secure and easy transactions.',
+            'thumbnail': 'Auto Gallery Automotive Contact Card.jpg',
+            'tags': 'Cryptocurrency, Wallet, Blockchain',
+            'createdAt': '2024-09-10 12:30:00',
+        },
+        {
+            'id': 2,
+            'title': 'The Future of Cryptocurrency',
+            'header': 'What the Future Holds for Crypto',
+            'body': 'The cryptocurrency market is rapidly evolving, and Konnex is at the forefront of the change.',
+            'thumbnail': 'Auto Gallery Automotive Contact Card.jpg',
+            'tags': 'Cryptocurrency, Future, Investment',
+            'createdAt': '2024-09-12 08:45:00',
+        },
+        {
+            'id': 3,
+            'title': 'Blockchain Basics',
+            'header': 'Understanding Blockchain Technology',
+            'body': 'Blockchain is the backbone of all cryptocurrency transactions, and Konnex leverages it to provide security.',
+            'thumbnail': 'Auto Gallery Automotive Contact Card.jpg',
+            'tags': 'Blockchain, Technology, Crypto',
+            'createdAt': '2024-09-13 14:00:00',
+        },
+        {
+            'id': 4,
+            'title': 'Security and Privacy in Cryptocurrency',
+            'header': 'Ensuring Your Digital Assets are Secure',
+            'body': 'With Konnex, security is our top priority. We use advanced encryption to protect your data and assets.',
+            'thumbnail': 'Auto Gallery Automotive Contact Card.jpg',
+            'tags': 'Security, Privacy, Cryptocurrency',
+            'createdAt': '2024-09-14 16:20:00',
+        }
+    ]
+
+
+
     # Fetch all blogs from the database using the class method get_all
-    blogs = Blog.get_all()
-    for blog in blogs:
-        if isinstance(blog['thumbnail'], bytes):
-            blog['thumbnail'] = blog['thumbnail'].decode('utf-8')  # Decode from bytes to string
-        print(f"Thumbnail: {blog['thumbnail']}")
-    # Detect if a blog is selected from the query parameter
+    # blogs = Blog.get_all()
+    # for blog in blogs:
+    #     if isinstance(blog['thumbnail'], bytes):
+    #         blog['thumbnail'] = blog['thumbnail'].decode('utf-8')  # Decode from bytes to string
+    #     print(f"Thumbnail: {blog['thumbnail']}")
+
+    # # Detect if a blog is selected from the query parameter
     selected_blog_id = request.args.get('blog_id')
 
-    # If a blog_id is provided, select the appropriate blog from the database
+    # # If a blog_id is provided, select the appropriate blog from the database
     selected_blog = None
     if selected_blog_id:
         selected_blog = next((blog for blog in blogs if blog['id'] == int(selected_blog_id)), None)
+
 
     # Return the appropriate template based on device type
     if device_type:
